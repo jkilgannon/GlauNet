0535e6b1e9ab44c7a6d3e905cc12fad8e736b761
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2020-02-09 04:55:18 -0500

    Update test.py

--- nn/test.py
+++ nn/test.py
@@ -56,11 +56,11 @@ def batchmaker_train():
             y_set = []
             for flname in x_files:
                 if not os.path.isdir(os.path.join(in_path_unseg, flname)):
-                    x_set.append(img_to_array(tf.image.resize(load_img(in_path_unseg + flname)), [320,480]))
+                    x_set.append(img_to_array(tf.image.resize(load_img(in_path_unseg + flname), [320,480]))
                     #x_set.append(img_to_array(load_img(in_path_unseg + flname)))
             for flname in y_files:
                 if not os.path.isdir(os.path.join(in_path_seg, flname)):
-                    y_set.append(img_to_array(tf.image.resize(load_img(in_path_seg + flname)), [320,480]))
+                    y_set.append(img_to_array(tf.image.resize(load_img(in_path_seg + flname), [320,480]))
                     #y_set.append(img_to_array(load_img(in_path_seg + flname)))
                     #y_set.append(load_img(in_path_seg + flname))
             
