b0d0fa22e2840fcd194cdc6e0cbfa7f98bc1d367
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2020-02-09 21:25:00 -0500

    Update testflow.py

--- nn/testflow.py
+++ nn/testflow.py
@@ -1,6 +1,7 @@
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import *
 from tensorflow.keras.layers import *
 from tensorflow.keras.optimizers import *
