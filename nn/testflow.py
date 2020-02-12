5c4cd91dcaacd0896e354aa5d2cfe17ada2ded0a
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2020-02-11 20:49:57 -0500

    shape

--- nn/testflow.py
+++ nn/testflow.py
@@ -77,9 +77,11 @@ up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 
 merge9 = concatenate([conv1,up9], axis = 3)
 conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
 conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+#conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+conv9 = Conv2D(3, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
 
-conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
+#conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
+conv10 = Conv2D(3, 1, activation = 'sigmoid')(conv9)
 
 model = Model(inputs = input_layer, outputs = conv10)
 
