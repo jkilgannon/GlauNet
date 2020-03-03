63572897b2623a8d3abc4a6cd83abde26aadfdb3
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2020-03-03 00:30:42 -0500

    Update count_classes.py

--- nn/count_classes.py
+++ nn/count_classes.py
@@ -7,18 +7,26 @@ input_file = fl_path + 'predicted.csv'
 f = open(input_file, "r")
 contents = f.read()
 elements = contents.split(",")	
+
 valid_list = []
 elements_list = []
-count = 0
+count_list = []
+
+totalcount = 0
 for num in elements:
-    count = count + 1
     h = hash(str(num))
     if not h in valid_list:
         valid_list.append(h)
         elements_list.append(num)
+        count_list.append(0)
+    
+    idx = valid_list.index(h)
+    count_list[idx] = count_list[idx] + 1
+    totalcount = totalcount + 1
 
 print(valid_list)
 print(elements_list)
-print(count)
+print(count_list)
+print(totalcount)
 
 print("Done")
