e883e966e06ccc734432b7fdfdfcf0fd41823a94
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2020-04-19 19:43:12 -0400

    Update singularitySetup.sh

--- singularity/singularitySetup.sh
+++ singularity/singularitySetup.sh
@@ -17,26 +17,53 @@ sudo pip3 install matplotlib
 sudo mkdir /worksite
 sudo mkdir /worksite/fundus/
 sudo mkdir /worksite/fundusvalidate/
-sudo mkdir /worksite/mask/
-sudo mkdir /worksite/maskvalidate/
+#sudo mkdir /worksite/mask/
+#sudo mkdir /worksite/maskvalidate/
+sudo mkdir /worksite/mask0/
+sudo mkdir /worksite/maskvalidate0/
+sudo mkdir /worksite/mask1/
+sudo mkdir /worksite/maskvalidate1/
+sudo mkdir /worksite/mask2/
+sudo mkdir /worksite/maskvalidate2/
 sudo mkdir /outgoing/
 
 sudo chmod 777 /worksite
 sudo chmod 777 /worksite/fundus
 sudo chmod 777 /worksite/fundusvalidate
-sudo chmod 777 /worksite/mask
-sudo chmod 777 /worksite/maskvalidate
+#sudo chmod 777 /worksite/mask
+#sudo chmod 777 /worksite/maskvalidate
+sudo chmod 777 /worksite/mask0
+sudo chmod 777 /worksite/maskvalidate0
+sudo chmod 777 /worksite/mask1
+sudo chmod 777 /worksite/maskvalidate1
+sudo chmod 777 /worksite/mask2
+sudo chmod 777 /worksite/maskvalidate2
 sudo chmod 777 /outgoing
 
 # Copy files
-sudo cp /local/repository/inprime/* /worksite/fundus/
-sudo cp /local/repository/inprimeval/* /worksite/fundusvalidate/
-sudo cp /local/repository/inseg/* /worksite/mask/
-sudo cp /local/repository/insegval/* /worksite/maskvalidate/
+#sudo cp /local/repository/inprime/* /worksite/fundus/
+#sudo cp /local/repository/inprimeval/* /worksite/fundusvalidate/
+#sudo cp /local/repository/inseg/* /worksite/mask/
+#sudo cp /local/repository/insegval/* /worksite/maskvalidate/
+sudo cp /local/repository/training_data/fundus/* /worksite/fundus/
+sudo cp /local/repository/training_data/fundusvalidate/* /worksite/fundusvalidate/
+sudo cp /local/repository/training_data/mask0/* /worksite/mask0/
+sudo cp /local/repository/training_data/mask1/* /worksite/mask1/
+sudo cp /local/repository/training_data/mask2/* /worksite/mask2/
+sudo cp /local/repository/training_data/maskvalidate0/* /worksite/maskvalidate0/
+sudo cp /local/repository/training_data/maskvalidate1/* /worksite/maskvalidate1/
+sudo cp /local/repository/training_data/maskvalidate2/* /worksite/maskvalidate2/
+
 sudo cp /local/repository/nn/* /users/jk880380/
 
 sudo chmod 777 /worksite/fundus/*
 sudo chmod 777 /worksite/fundusvalidate/*
-sudo chmod 777 /worksite/mask/*
-sudo chmod 777 /worksite/maskvalidate/*
+#sudo chmod 777 /worksite/mask/*
+#sudo chmod 777 /worksite/maskvalidate/*
+sudo chmod 777 /worksite/mask0/*
+sudo chmod 777 /worksite/maskvalidate0/*
+sudo chmod 777 /worksite/mask1/*
+sudo chmod 777 /worksite/maskvalidate1/*
+sudo chmod 777 /worksite/mask2/*
+sudo chmod 777 /worksite/maskvalidate2/*
 sudo chmod 777 /users/jk880380/*
