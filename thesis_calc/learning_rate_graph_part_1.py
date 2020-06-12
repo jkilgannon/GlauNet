a9edfc1de69877507902769a6c786568abaa7160
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2020-06-12 07:06:41 -0400

    add incorrect & aggregate correctness graphs

--- thesis_calc/learning_rate_graph_part_1.py
+++ thesis_calc/learning_rate_graph_part_1.py
@@ -41,6 +41,7 @@ y_len = len(ground_truth[0])
 # Master list of correctnesses
 correctness_for_LRs = []
 incorrectness_for_LRs = []
+aggregate_correctness_for_LRs = []
 
 # This is the learning rate being worked on.
 all_learning_rates = ['2', '3', '4', '5', '5x6' , '6', '7']
@@ -64,6 +65,7 @@ for learning_rate in all_learning_rates:
     # We will contain all the "correctness" data in master lists.
     correctness_data = []       # Correct pixels per epoch
     incorrectness_data = []     # Incorrect pixels per epoch
+    aggregate_correctness_data = []     # # correct minus # incorrect
     
     #current_epoch = 0
     
@@ -98,9 +100,11 @@ for learning_rate in all_learning_rates:
         print(target + ", learning rate: " + str(learning_rate) + ", total correct: " + str(total_correct) + ", total incorrect: " + str(total_incorrect))
         correctness_data.append(total_correct)
         incorrectness_data.append(total_incorrect)
+        aggregate_correctness_data.append(total_correct - total_incorrect)
 
     correctness_for_LRs.append(correctness_data)
     incorrectness_for_LRs.append(incorrectness_data)
+    aggregate_correctness_for_LRs.append(aggregate_correctness_data)
     
 # We now have a list of the number of correct/incorrect pixels in each image 
 # Element n contains the correctness data for epoch n+1 for the given learning rate.
@@ -109,16 +113,20 @@ for learning_rate in all_learning_rates:
 # See https://pundit.pratt.duke.edu/wiki/Python:Plotting_Surfaces for some
 # guidance (but not much guidance, sadly).
 
-# X-axis: the learning rates.  
+# Y-axis: the learning rates.  
 # One of the learning rates is fractional (5 * 10e-6); give it a 
 # representation on a log-10 scale.  log_10(5*10e-6) = 5.301
 LRs = np.array([2, 3, 4, 5, 5.3, 6, 7])
 
-# Y-axis: the epochs
+# X-axis: the epochs
 epochs = np.array(list(range(1,max_epoch+1)))
 
 (x, y) = np.meshgrid(epochs, LRs)
-z = np.array(correctness_for_LRs)
+
+# ******************* GRAPH BOTH THE CORRECTNESS AND  INCORRECTNESS DATA! *********************************************
+#z = np.array(correctness_for_LRs)
+#z = np.array(incorrectness_for_LRs)
+z = np.array(aggregate_correctness_for_LRs)
 
 # Make a 3D display
 fig = plt.figure(num=1, clear=True)
@@ -126,7 +134,10 @@ ax = fig.add_subplot(1, 1, 1, projection='3d')
 
 # Plot the data on it.
 ax.plot_surface(x, y, z)
-ax.set(xlabel='Epoch', ylabel='-log_10(Learning Rate)', zlabel='Correct Pixels', title='Correctness by Learning Rate and Epoch')
+
+#ax.set(xlabel='Epoch', ylabel='-log_10(Learning Rate)', zlabel='Correct Pixels', title='Correctness by Learning Rate and Epoch')
+#ax.set(xlabel='Epoch', ylabel='-log_10(Learning Rate)', zlabel='Incorrect Pixels', title='Incorrectness by Learning Rate and Epoch')
+ax.set(xlabel='Epoch', ylabel='-log_10(Learning Rate)', zlabel='Aggregate Correct Pixels', title='Aggregate Correctness by Learning Rate and Epoch')
 
 fig.tight_layout()
 #fig.savefig('PatchExOrig_py.png')
@@ -134,8 +145,5 @@ fig.tight_layout()
 # Show the graph!
 plt.show()
 
-# ******************* NOW GRAPH THE INCORRECTNESS DATA! *********************************************
-
     
 print("Done.")
-
